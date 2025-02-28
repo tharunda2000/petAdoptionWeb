bf988632ecaa46beddbb3c42ec7efe5a3bad89b3
Tharunda De Silva <tharundadesilva@gmail.com>
2025-03-01 02:36:40 +0530

    api connected

---
+++ app.js
@@ -0,0 +1,27 @@
+
+let body = "";
+
+fetch(" https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_4PosqEp4RTatBZ2MLopLSTDBcj76ADSYSqPVwKLXvO1H1clQVdpFpTk5E56bTWSR")
+  .then(res => res.json())
+  .then(data => {
+
+    data.forEach(element => {
+
+        body+=`<div class="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex justify-content-center">
+            <div class="card" style="width: 383px;">
+            <img src="${element.url}" class="card-img-top" id="pet-img" alt="...">
+            <div class="card-body">
+            <button type="button" class="btn btn-outline-danger">Adopt Now</button>
+            <button type="button" class="btn btn-outline-warning">View details</button>
+            </div>
+          </div>
+          </div>`
+        console.log(element);
+
+    });
+
+    document.getElementById("pet").innerHTML=body;
+
+  })
+
+  
