b812450023f17821dac4804f288562d0479af19c
Tharunda De Silva <tharundadesilva@gmail.com>
2025-03-01 12:13:20 +0530

    animations added for pet images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 let body = "";
 
-fetch(" https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_4PosqEp4RTatBZ2MLopLSTDBcj76ADSYSqPVwKLXvO1H1clQVdpFpTk5E56bTWSR")
+fetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_4PosqEp4RTatBZ2MLopLSTDBcj76ADSYSqPVwKLXvO1H1clQVdpFpTk5E56bTWSR")
   .then(res => res.json())
   .then(data => {
 
@@ -9,7 +9,14 @@ fetch(" https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_k
 
         body+=`<div class="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex justify-content-center">
             <div class="card" style="width: 383px;">
-            <img src="${element.url}" class="card-img-top" id="pet-img" alt="...">
+
+            <div class="image-zoom">
+            <div class="image-zoom-wrapper">
+              <img src="${element.url}" class="card-img-top" id="pet-img" alt="...">
+            </div>
+            </div>
+
+
             <div class="card-body">
             <button type="button" class="btn btn-outline-danger">Adopt Now</button>
             <button type="button" class="btn btn-outline-warning">View details</button>
